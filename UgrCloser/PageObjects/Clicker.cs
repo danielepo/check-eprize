1553a4fd2e165c60024580444c557f46bf7249c0
danielepo <dnl.pozzobon@gmail.com>
2017-11-10 16:18:14 +0100

    changed to cliccker

---
+++ UgrCloser/PageObjects/Clicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+
+namespace UgrCloser.PageObjects
+{
+    class Clicker
+    {
+        private readonly IWebDriver _driver;
+        string _homeHandle;
+        private int _retries = 0;
+        public Clicker(IWebDriver driver)
+        {
+            _driver = driver;
+            _retries = 0;
+        }
+
+        public Clicker Go()
+        {
+            _driver.Navigate().GoToUrl("https://www.eprice.it/black-hour");
+
+            return this;
+        }
+
+        private void UseTopPanel()
+        {
+            var topRight = _driver.FindElements(By.TagName("frame")).First(x => x.GetAttribute("name") == "TopRight");
+            _driver.SwitchTo().Frame(topRight);
+        }
+        public Clicker Search()
+        {
+            UseTopPanel();
+            var query = _driver.FindElement(By.CssSelector(".item:first-child .btn"));
+            //var query = _driver.FindElement(By.CssSelector(".item.superSconto:first-child .btn"));
+            query.Click();
+            return this;
+        }
+        
+    }
+}
---
+++ UgrCloser/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using UgrCloser.PageObjects;
+
+namespace UgrCloser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new Proxy
+            {
+                HttpProxy = "http://eul0856:Dony2211!@proxyfull.servizi.ras:80",
+                SslProxy = "http://eul0856:Dony2211!@proxyfull.servizi.ras:80"
+            };
+
+            var options = new ChromeOptions
+            {
+                Proxy = proxy
+            };
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+
+                //Notice navigation is slightly different than the Java version
+                //This is because 'get' is a keyword in C#
+                var ugrMain = new Clicker(driver);
+
+                while (true)
+                {
+                    ugrMain.Go().Search();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private static void closeFor(IWebDriver driver, string app)
+        {
+
+        }
+    }
+}
